97e95c90c2ece7e9f3c51820286c7e1beadb4f98
Matheus Stein Fontana <matheusfontana>
2016-11-13 14:09:28 -0200

    Database structure

--- db/migrations/20161113064313_survey_table.php
+++ db/migrations/20161113064313_survey_table.php
@@ -4,29 +4,50 @@ use Phinx\Migration\AbstractMigration;
 
 class SurveyTable extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
-    public function change()
-    {
+	/**
+	 * Change Method.
+	 *
+	 * Write your reversible migrations using this method.
+	 *
+	 * More information on writing migrations is available here:
+	 * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+	 *
+	 * The following commands can be used in this method and Phinx will
+	 * automatically reverse them when rolling back:
+	 *
+	 *    createTable
+	 *    renameTable
+	 *    addColumn
+	 *    renameColumn
+	 *    addIndex
+	 *    addForeignKey
+	 *
+	 * Remember to call "create()" or "update()" and NOT "save()" when working
+	 * with the Table class.
+	public function change()
+	{
 
-    }
+	}
+	*/
+
+	/**
+	 * Migrate up
+	 * @return void
+	 */
+	public function up()
+	{
+		$survey = $this->table('survey', array('engine' => 'InnoDB'));
+		$survey->addColumn('template_survey_id', 'integer', array('limit' => 11))
+			->addForeignKey('template_survey_id', 'template_survey', 'id', array('delete' => 'RESTRICT', 'update' => 'RESTRICT'))
+			->save();
+	}
+
+	/**
+	 * Migrate down
+	 * @return void
+	 */
+	public function down()
+	{
+		$this->dropTable('survey');
+	}
 }
--- db/migrations/20161113064336_survey_answer_table.php
+++ db/migrations/20161113064336_survey_answer_table.php
@@ -4,29 +4,52 @@ use Phinx\Migration\AbstractMigration;
 
 class SurveyAnswerTable extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
-     *
-     * The following commands can be used in this method and Phinx will
-     * automatically reverse them when rolling back:
-     *
-     *    createTable
-     *    renameTable
-     *    addColumn
-     *    renameColumn
-     *    addIndex
-     *    addForeignKey
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
-    public function change()
-    {
+	/**
+	 * Change Method.
+	 *
+	 * Write your reversible migrations using this method.
+	 *
+	 * More information on writing migrations is available here:
+	 * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+	 *
+	 * The following commands can be used in this method and Phinx will
+	 * automatically reverse them when rolling back:
+	 *
+	 *    createTable
+	 *    renameTable
+	 *    addColumn
+	 *    renameColumn
+	 *    addIndex
+	 *    addForeignKey
+	 *
+	 * Remember to call "create()" or "update()" and NOT "save()" when working
+	 * with the Table class.
+	public function change()
+	{
 
-    }
+	}
+	*/
+
+	/**
+	 * Migrate up
+	 * @return void
+	 */
+	public function up()
+	{
+		$idSurvey = $this->table('survey_answer', array('engine' => 'InnoDB'));
+		$idSurvey->addColumn('answer_id', 'integer', array('limit' => 11))
+			->addColumn('survey_id', 'integer', array('limit' => 11))
+			->addForeignKey('answer_id', 'answer', 'id', array('delete' => 'RESTRICT', 'update' => 'RESTRICT'))
+			->addForeignKey('survey_id', 'survey', 'id', array('delete' => 'RESTRICT', 'update' => 'RESTRICT'))
+			->save();
+	}
+
+	/**
+	 * Migrate down
+	 * @return void
+	 */
+	public function down()
+	{
+		$this->dropTable('survey_answer');
+	}
 }
